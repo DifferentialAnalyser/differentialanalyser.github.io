062e65f4256a4cb1984d22bf73fffaec95ebdf4d
hanzhangshen03 <shenhanzhang@outlook.com>
2025-02-02 16:04:26 +0000

    feat(Integrator): implement the integrator class

--- src/core/Integrator.ts
+++ src/core/Integrator.ts
@@ -1,24 +1,51 @@
+/**
+ * @file Integrator.ts
+ * @description This file contains the definition of the Integrator class, which implements the Device interface.
+ * The Integrator class is responsible for integrating the input shafts and providing an output shaft.
+ * @author Hanzhang Shen
+ */
+
 import { Device } from "./Device";
 import { Shaft } from "./Shaft";
 
+/**
+ * @class Integrator
+ * @description The Integrator class is responsible for integrating the input shafts and providing an output shaft.
+ * @implements Device
+ */
 export class Integrator implements Device {
     private output: Shaft;
-    private disk_position: number;
     private integrand: Shaft;
-    private variable_of_integration: Shaft;
+    private variableOfIntegration: Shaft;
+    private readonly wheelRadius: number = 1;
+
+    /**
+     * @constructor
+     * @description The constructor of the Integrator class.
+     * @param variableOfIntegration The shaft that represents the differential dx.
+     * @param integrand The shaft that represents the integrand f(x).
+     * @param output The output shaft that represents the angular speed of the wheel.
+     */
     constructor(
-        variable_of_integration: Shaft, 
-        integrand: Shaft, 
-        output: Shaft, 
-        initial_disk_position: number
+        variableOfIntegration: Shaft,
+        integrand: Shaft,
+        output: Shaft,
     ) {
         this.output = output;
         this.integrand = integrand;
-        this.disk_position = initial_disk_position;
-        this.variable_of_integration = variable_of_integration;
+        this.variableOfIntegration = variableOfIntegration;
     }
+
+    /**
+     * @method getOutput
+     * @description This method calculates the angular speed of the wheel by integrating the input shafts.
+     * @returns The output shaft that represents the angular speed of the wheel.
+     */
     getOutput(): Shaft | undefined {
-        this.disk_position += this.integrand.current_rotation;
+        // Linear speed of the wheel = f(x) * dx.
+        let wheelLinearSpeed: number = this.variableOfIntegration.current_rotation * this.integrand.current_rotation;
+        let wheelAngularSpeed: number = wheelLinearSpeed / this.wheelRadius;
+        this.output.next_rotation = wheelAngularSpeed;
         return this.output;
     }
 }
