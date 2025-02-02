a6aaf410843c2afad95942cb36520bcf45940dbb
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2025-02-02 11:08:55 +0000

    implemented function table/output table getOutput

--- src/core/FunctionTable.ts
+++ src/core/FunctionTable.ts
@@ -3,6 +3,7 @@ import { Shaft } from "./Shaft";
 
 export class FunctionTable extends Device {
     output: Shaft;
+    Xposition: number;
     fun: (n: number) => number;
     constructor(inputs: Shaft[], output: Shaft, initial: number, fun: (n: number) => number) {
         if(inputs.length != 1) {
@@ -11,9 +12,14 @@ export class FunctionTable extends Device {
         super(inputs, initial);
         this.output = output;
         this.fun = fun;
+        this.Xposition = initial;
     }
     getOutput(): Shaft | undefined {
-        // TODO: finish function table;
+        if(!this.output.result_ready) {
+            this.Xposition += this.shafts[0].next_rotation;
+            this.output.result_ready = true;
+            this.output.next_rotation = this.fun(this.Xposition);
+        }
         return this.output;
     }
 }
--- src/core/OutputTable.ts
+++ src/core/OutputTable.ts
@@ -1,13 +1,14 @@
 import { Shaft } from "./Shaft";
+import { Device } from "./Device";
 
-export class OutputTable {
-    inputs: Shaft[];
-    initial: number;
+export class OutputTable extends Device {
     constructor(inputs: Shaft[], initial: number) {
         if(inputs.length != 3) {
             throw new Error("Wrong Length for OutputTable");
         }
-        this.inputs = inputs;
-        this.initial = initial;
+        super(inputs, initial);
+    }
+    getOutput(): Shaft | undefined {
+        return undefined;
     }
 }
