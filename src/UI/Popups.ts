d924e387e6fe81ce5d02d8cec5632b26288d17dc
Aaron Danton <aarondanton04@hotmail.com>
2025-02-06 21:22:25 +0000

    Added functionality for gear and multiplier popups

--- src/UI/Popups.ts
+++ src/UI/Popups.ts
@@ -15,30 +15,40 @@ function openPopup(e: MouseEvent, popup: HTMLDivElement): void {
   popup.style.left = `${e.clientX + 10}px`;
   popup.style.top = `${e.clientY + 10}px`;
   popup.style.zIndex = "10";
+
+  const target: DraggableComponentElement = e.currentTarget as DraggableComponentElement;
+  popup.dataset.id = target.id;
 }
 
 export function openShaftPopup(e: MouseEvent): void {
   openPopup(e, shaftPopup);
 
-  const target: DraggableComponentElement = e.currentTarget as DraggableComponentElement;
-  const inputs = shaftPopup.getElementsByTagName("input") as HTMLCollectionOf<HTMLInputElement>;
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].value = "0";
-  }
-  shaftPopup.dataset.id = target.id;
-
   e.preventDefault();
 }
 
 export function openGearPopup(e: MouseEvent): void {
-  // TODO: Setup gear settings 
   openPopup(e, gearPopup);
+
+  const target = e.currentTarget as DraggableComponentElement;
+  const inputs = gearPopup.getElementsByTagName("input");
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i] as HTMLInputElement;
+    if (input.id == "gear-input-ratio") {
+      input.value = String(target.inputRatio);
+    }
+    else if (input.id == "gear-output-ratio") {
+      input.value = String(target.outputRatio);
+    }
+  }
 
   e.preventDefault();
 }
 
 export function openMultiplierPopup(e: MouseEvent): void {
   openPopup(e, multiplierPopup);
+
+  const target = e.currentTarget as DraggableComponentElement;
+  multiplierPopup.getElementsByTagName("input")[0].value = String(target.outputRatio);
 
   e.preventDefault();
 }
@@ -54,19 +64,19 @@ function setupShaftPopup(): void {
   const buttons = shaftPopup.getElementsByTagName("button");
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", (e) => {
-      const input: HTMLInputElement = e.currentTarget as HTMLInputElement;
-      const component = document.getElementById(input.parentElement!.parentElement!.dataset.id!) as DraggableComponentElement;
+      const button: HTMLButtonElement = e.currentTarget as HTMLButtonElement;
+      const component = document.getElementById(button.parentElement!.parentElement!.dataset.id!) as DraggableComponentElement;
 
       let negativeLength = 0;
-      if (input.id == "shaft-popup-negative-increase") negativeLength = 1;
-      if (input.id == "shaft-popup-negative-decrease") negativeLength = -1;
+      if (button.id == "shaft-popup-negative-increase") negativeLength = 1;
+      if (button.id == "shaft-popup-negative-decrease") negativeLength = -1;
 
       let positiveLength = 0;
-      if (input.id == "shaft-popup-positive-increase") positiveLength = 1;
-      if (input.id == "shaft-popup-positive-decrease") positiveLength = -1;
+      if (button.id == "shaft-popup-positive-increase") positiveLength = 1;
+      if (button.id == "shaft-popup-positive-decrease") positiveLength = -1;
 
       updateShaftLength(component, negativeLength, positiveLength);
-    })
+    });
   }
 }
 
@@ -74,11 +84,37 @@ function setupShaftPopup(): void {
 function setupGearPopup(): void {
   gearPopup = document.getElementById("gear-popup") as HTMLDivElement;
   gearPopup.addEventListener("mouseleave", closePopup);
+
+  const inputs = gearPopup.getElementsByTagName("input");
+  console.log(inputs);
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("change", (e) => {
+      const input: HTMLInputElement = e.currentTarget as HTMLInputElement;
+      const component = document.getElementById(input.parentElement!.parentElement!.dataset.id!) as DraggableComponentElement;
+
+      if (input.id == "gear-input-ratio") {
+        component.inputRatio = Number(input.value);
+      } else if (input.id == "gear-output-ratio") {
+        component.outputRatio = Number(input.value);
+      }
+    })
+  }
 }
 
 function setupMultiplierPopup(): void {
   multiplierPopup = document.getElementById("multiplier-popup") as HTMLDivElement;
   multiplierPopup.addEventListener("mouseleave", closePopup);
+
+  const inputs = multiplierPopup.getElementsByTagName("input");
+  console.log(inputs);
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("change", (e) => {
+      const input: HTMLInputElement = e.currentTarget as HTMLInputElement;
+      const component = document.getElementById(input.parentElement!.dataset.id!) as DraggableComponentElement;
+
+      component.outputRatio = Number(input.value);
+    })
+  }
 }
 
 function updateShaftLength(comp: DraggableComponentElement, negativeLength: number, positiveLength: number) {
