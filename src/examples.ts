9db3e1a28234d755c682916ac301dc6bfa72f331
Aaron Danton <aarondanton04@hotmail.com>
2025-02-13 16:38:49 +0000

    Moved examples to dedicated examples folder

--- src/examples.ts
+++ src/examples.ts
@@ -1,7 +1,7 @@
 import { Config } from "./config";
 
-import _integrating_linear from "../integrating-linear.json";
-import _example_config from "../ExampleConfig.json";
+import _integrating_linear from "../examples/integrating-linear.json";
+import _example_config from "../examples/ExampleConfig.json";
 
 export const EXAMPLE_CONFIG: Config = _example_config as any;
 export const INTEGRATING_LINEAR: Config = _integrating_linear as any;
