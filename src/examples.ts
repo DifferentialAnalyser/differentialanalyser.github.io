baf1043690c25545980748633b4f477367a3d269
Joseph Hunt <josephcahunt@gmail.com>
2025-02-17 12:30:03 +0000

    Add broken config to examples.ts

--- src/examples.ts
+++ src/examples.ts
@@ -5,3 +5,7 @@ import _example_config from "../examples/spring-example.json";
 
 export const EXAMPLE_CONFIG: Config = _example_config as any;
 export const INTEGRATING_LINEAR: Config = _integrating_linear as any;
+
+import _dangling_shaft from "../examples/broken/dangling-shaft.json";
+
+export const BROKEN_DANGLING_SHAFT: Config = _dangling_shaft as any;
