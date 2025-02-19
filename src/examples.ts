53991d9f37ec43e7300acab6e2a61cc6f219565e
hanzhangshen03 <shenhanzhang@outlook.com>
2025-02-19 22:07:33 +0000

    feat: add a test for another oscillation equation

--- src/examples.ts
+++ src/examples.ts
@@ -8,6 +8,7 @@ import _gear_pair_example from "../examples/gear-pair.json";
 import _epicycloid_example from "../examples/integrating-epicycloid.json";
 import _extreme_epicycloid from "../examples/epicycloid-from-components.json";
 import _freefall_example from "../examples/body-in-free-fall.json";
+import _oscillation_example from "../examples/spring-example-2.json"
 
 export const SPRING_EXAMPLE: Config = _spring_example as any;
 export const LINEAR_INTEGRATION_EXAMPLE: Config = _linear_integration_example as any;
@@ -17,6 +18,7 @@ export const GEAR_PAIR_EXAMPLE: Config = _gear_pair_example as any;
 export const EPICYCLOID_EXAMPLE: Config = _epicycloid_example as any;
 export const EXTREME_EPICYCLOID_EXAMPLE: Config = _extreme_epicycloid as any;
 export const FREE_FALL_EXAMPLE: Config = _freefall_example as any;
+export const OSCILLATION_EXAMPLE: Config = _oscillation_example as any;
 
 import _dangling_shaft from "../examples/broken/dangling-shaft.json";
 
