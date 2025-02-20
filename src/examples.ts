a3ee336e8a150e32f5e99cc97b31469f5c36bda7
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2025-02-20 10:10:44 +0000

    added population growth as example

--- src/examples.ts
+++ src/examples.ts
@@ -8,8 +8,9 @@ import _gear_pair_example from "../examples/gear-pair.json";
 import _epicycloid_example from "../examples/integrating-epicycloid.json";
 import _extreme_epicycloid from "../examples/epicycloid-from-components.json";
 import _freefall_example from "../examples/body-in-free-fall.json";
-import _oscillation_example from "../examples/spring-example-2.json"
-import _duffing_equation_example from "../examples/duffing-equation.json"
+import _oscillation_example from "../examples/spring-example-2.json";
+import _duffing_equation_example from "../examples/duffing-equation.json";
+import _population_growth_example from "../examples/population-growth.json";
 
 export const SPRING_EXAMPLE: Config = _spring_example as any;
 export const LINEAR_INTEGRATION_EXAMPLE: Config = _linear_integration_example as any;
@@ -21,6 +22,7 @@ export const EXTREME_EPICYCLOID_EXAMPLE: Config = _extreme_epicycloid as any;
 export const FREE_FALL_EXAMPLE: Config = _freefall_example as any;
 export const OSCILLATION_EXAMPLE: Config = _oscillation_example as any;
 export const DUFFING_EQUATION_EXAMPLE: Config = _duffing_equation_example as any;
+export const POPULATION_GROWTH_EXAMPLE: Config = _population_growth_example as any;
 
 import _dangling_shaft from "../examples/broken/dangling-shaft.json";
 
